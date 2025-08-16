ff3eb461f4d9eaea0fe6eb38cfce13f78ff0cbdc
kubra islami <kubra.islami.00@gmail.com>
2025-08-16 11:59:13 +0100

    the user added to db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import {Pool} from "pg";
+
+
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -6,10 +10,6 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-    max: 5,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000,
-})
-console.log("DB Password:", process.env.DB_PASSWORD);
+});
 
 export default pool;
--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from "express";
+import userRoutes from './routes/users.js';
 const app = express();
-
-
 const PORT = process.env.PORT || 3000;
 
-import taskRoutes from './routes/tasks.js';
-import userRoutes from './routes/users.js';
-
-
-
 app.use(express.json());
-// app.use("/task",taskRoutes);
-app.use("/user",userRoutes);
-
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// console.log("index.js");
