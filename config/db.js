2b2efc9033ea6718319679c784a307bfc2dda66f
kubra islami <kubra.islami.00@gmail.com>
2025-08-19 18:41:35 +0100

    the origin set to * on coresOprions file

--- config/db.js
+++ config/db.js
@@ -2,21 +2,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 import {Pool} from "pg";
 
-// const pool = new Pool(
-//     process.env.DATABASE_URL
-//         ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
-//         : {
-//             user: process.env.DB_USER,
-//             password: process.env.DB_PASSWORD,
-//             host: process.env.DB_HOST,
-//             port: process.env.DB_PORT,
-//             database: process.env.DB_NAME,
-//         }
-// );
+const pool = new Pool(
+    process.env.DATABASE_URL
+        ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+        : {
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+        }
+);
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-});
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+// });
 
 export default pool;
--- coresOptions.js
+++ coresOptions.js
@@ -1,9 +1,9 @@
 const corsOptions = {
-    origin: [
-        'http://localhost:3000',
-        'https://task-manager-9vcy-git-main-kobra-eslamis-projects.vercel.app/'
-    ],
-    // origin: "*",
+    // origin: [
+    //     'http://localhost:3000',
+    //     'https://task-manager-9vcy-git-main-kobra-eslamis-projects.vercel.app/'
+    // ],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 };
