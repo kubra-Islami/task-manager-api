9170d4997fcc3b028e0c464749195dfc2ed2762b
kubra islami <kubra.islami.00@gmail.com>
2025-08-19 18:49:16 +0100

    the DATABASE_URL was changed

--- coresOptions.js
+++ coresOptions.js
@@ -1,9 +1,8 @@
 const corsOptions = {
-    // origin: [
-    //     'http://localhost:3000',
-    //     'https://task-manager-9vcy-git-main-kobra-eslamis-projects.vercel.app/'
-    // ],
-    origin: "*",
+    origin: [
+        'https://task-manager-9vcy-git-main-kobra-eslamis-projects.vercel.app/'
+    ],
+    // origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 };
