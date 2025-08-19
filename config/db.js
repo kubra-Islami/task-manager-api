adf4446eb49956d9c954d3cbc6fa143d9b359973
kubra islami <kubra.islami.00@gmail.com>
2025-08-19 17:07:03 +0100

    the DATABASE_URL was changed

--- config/db.js
+++ config/db.js
@@ -2,16 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 import {Pool} from "pg";
 
-const pool = new Pool(
-    process.env.DATABASE_URL
-        ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
-        : {
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            database: process.env.DB_NAME,
-        }
-);
+// const pool = new Pool(
+//     process.env.DATABASE_URL
+//         ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+//         : {
+//             user: process.env.DB_USER,
+//             password: process.env.DB_PASSWORD,
+//             host: process.env.DB_HOST,
+//             port: process.env.DB_PORT,
+//             database: process.env.DB_NAME,
+//         }
+// );
+
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
