2c38219ecc6d72a68b61dc404fb4078463c39620
kubra islami <kubra.islami.00@gmail.com>
2025-08-19 17:12:33 +0100

    the DATABASE_URL was changed

--- config/db.js
+++ config/db.js
@@ -14,8 +14,6 @@ import {Pool} from "pg";
 //         }
 // );
 
-
-const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
